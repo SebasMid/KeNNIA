d0b8c2728b70de53b18481e4d0786ce554a6143d
Sebastian Abdala Asencio <midnanohime@gmail.com>
2023-12-04 22:35:51 -0500

    autores.txt

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -6,6 +6,11 @@ function DashboardPage() {
     <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <div>
         <h1 className="text-white text-5xl">Dashboard</h1>
+        <a href='http://127.0.0.1:5000/'>
+        <button className="w-full bg-blue-500 text-white p-3 rounded-lg mt-2">
+          Registrar
+        </button>
+        </a>
       </div>
     </section>
   )
