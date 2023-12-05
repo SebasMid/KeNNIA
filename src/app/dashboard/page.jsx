19069ba58cdbb9f14ceb05b941e454a77206fa9c
Sebastian Abdala Asencio <midnanohime@gmail.com>
2023-12-04 22:42:08 -0500

    autores.txt

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,10 +5,10 @@ function DashboardPage() {
   return (
     <section className="h-[calc(100vh-7rem)] flex justify-center items-center">
       <div>
-        <h1 className="text-white text-5xl">Dashboard</h1>
+        <h1 className="text-white text-5xl">EStás a un paso de KennIA!</h1>
         <a href='http://127.0.0.1:5000/'>
         <button className="w-full bg-blue-500 text-white p-3 rounded-lg mt-2">
-          Registrar
+          Ir a kennia
         </button>
         </a>
       </div>
